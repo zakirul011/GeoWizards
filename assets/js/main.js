115a22a34d263d431bef6c41814c7190b43be680
Zakirul Islam <zakirul011@gmail.com>
2024-10-04 16:38:26 +0600

    Added popup to interconnection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,6 @@
-// async function fetchRealTimeData() {
-//   const lon = -95.33;
-//   const lat = 29.78;
-//   const dim = 0.1;
-//   const date = "2022-01-01";
-//   const apiKey = "aSn4OfEAV4FJRG13XD8XetXFmwnIB4IegowI3W8I";
-
-//   const url = `https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=${dim}&api_key=${apiKey}`;
-
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//   }
-// }
-
-// fetchRealTimeData();
-
-//// Replace with your Earthdata username, password, and the dataset URL
-// const username = "zakirul011";
-// const password = "#EaZakirul[011]";
-// const datasetUrl =
-//   "https://data.nasa.gov/resource/global-surface-temperature.json";
-
-// async function fetchTemperatureData() {
-//   const headers = new Headers();
-//   headers.set("Authorization", "Basic " + btoa(username + ":" + password));
-
-//   try {
-//     const response = await fetch(datasetUrl, {
-//       method: "GET",
-//       headers: headers,
-//     });
-
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok " + response.statusText);
-//     }
-
-//     const data = await response.json();
-//     console.log("Temperature Data:", data);
-
-//     // Implement how this data will impact the systems
-//     // updateSystemsWithTemperature(data);
-//   } catch (error) {
-//     console.error("Error fetching temperature data:", error);
-//   }
-// }
-
-// fetchTemperatureData();
-
 // HERO BUTTON TO FEATURES
+const homeBtn = document.querySelector(".home-button");
+
 const hero = document.querySelector(".hero-content");
 const features = hero.querySelectorAll(".feature-item");
 const expBtn = hero.querySelector(".explore-btn");
@@ -66,7 +13,6 @@ const activitiesItem = hero.querySelectorAll(".activities-item");
 const systemTabs = hero.querySelectorAll(".system-tab");
 const activitiesTabs = hero.querySelectorAll(".activities-tab");
 const featureTabs = hero.querySelectorAll(".feature-tab");
-const homeBtn = document.querySelector(".home-button");
 
 expBtn.addEventListener("click", () => {
   hero.classList.add("animation-1");
@@ -328,6 +274,15 @@ connectionWrap.forEach((wrap) => {
   let introBtn = wrap.querySelector(".connection-intro button");
   let markers = wrap.querySelectorAll(".connection-marker");
   let titleClicked = false;
+  let timeline = [];
+  let popups = wrap.querySelectorAll(".connection-popup");
+
+  popups.forEach((popup) => {
+    let close = popup.querySelector(".connection-popup-close");
+    close.addEventListener("click", () => {
+      popup.classList.remove("active");
+    });
+  });
 
   let standard = {
     min: 0,
@@ -472,7 +427,42 @@ connectionWrap.forEach((wrap) => {
     updateGlacier();
     updateLava();
     updateWater();
+    updatePopup();
   }
+
+  function updatePopup() {
+    let percentSystem = {
+      temperature: getPercentage(system.atm.temperature),
+      water: getPercentage(system.hyd.water),
+      glacier: getPercentage(system.cry.glacier),
+      coastal: getPercentage(system.geo.coastal),
+      trees: getPercentage(system.bio.trees),
+    };
+
+    for (let i = 0; i < popups.length; i++) {
+      let key = Object.keys(percentSystem)[i];
+      let value = percentSystem[key];
+      let textUpdate = popups[i].querySelector(".connection-popup-update");
+
+      clearTimeout(timeline[i]);
+      popups[i].classList.add("active");
+      if (value > 0.5) {
+        textUpdate.textContent = "increasing";
+      } else if (value > 0) {
+        textUpdate.textContent = "Decreasing";
+      }
+
+      timeline[i] = setTimeout(() => {
+        popups[i].classList.remove("active");
+      }, 3000);
+    }
+
+    function getPercentage(element) {
+      let value = (element.value - element.min) / (element.max - element.min);
+      return value;
+    }
+  }
+
   function updateSystemObejct() {
     let systemKeysArray = Object.keys(system);
 
@@ -720,174 +710,8 @@ headerArea.forEach((area) => {
 });
 //========== STICKY HEADER, BACK TO TOP// ==========>
 
-// lottieIcon
-const lottieIcon = document.querySelectorAll(".lottie-icon");
-
-lottieIcon.forEach((icon) => {
-  let src = icon.getAttribute("src");
-
-  let animation = bodymovin.loadAnimation({
-    container: icon,
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: src,
-  });
-  icon.addEventListener("mouseenter", function () {
-    animation.play();
-  });
-  icon.addEventListener("mouseleave", function () {
-    animation.stop();
-  });
-});
-
-// // gsap
-// ScrollTrigger.create({
-//   trigger: ".hero-area",
-//   start: "top top",
-//   end: "+=1500",
-//   pin: ".hero-content",
-// });
-
-// gsap.to(".hero-text", {
-//   scale: 0.5,
-//   xPercent: -30,
-//   yPercent: 20,
-//   opacity: 0,
-
-//   scrollTrigger: {
-//     trigger: ".hero-area",
-//     start: "top top",
-//     end: "+=800",
-//     scrub: 1,
-//   },
-// });
-
-// // Create a timeline with a ScrollTrigger instance
-// const tl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".hero-area",
-//     start: "top top",
-//     end: "+=1500",
-//     scrub: 1,
-//   },
-// });
-
-// // First animation: Scale down and move the globe slightly
-// tl.fromTo(
-//   ".globe",
-//   {
-//     scale: 2,
-//   },
-//   {
-//     scale: 1,
-//     xPercent: -5,
-//     yPercent: -80,
-//     duration: 1,
-//     ease: "power1.inOut",
-//   }
-// );
-
-// tl.to({}, { duration: 2 });
-
-// // Second animation: Move the globe to the right after the gap
-// // tl.to(".globe", {
-// //   xPercent: 124,
-// //   duration: 0.5,
-// //   ease: "power1.inOut",
-// // });
-
-// // features aniamtion
-// const featureTl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".hero-area",
-//     start: "500 top",
-//     end: "+=1000", // Total duration combining both animations (1200px + 800px)
-//     scrub: 1,
-//   },
-// });
-
-// // First animation: Fade in and move elements to the left
-// featureTl.fromTo(
-//   ".feature-item",
-//   { opacity: 0, x: 100, pointerEvents: "none" },
-//   {
-//     opacity: 1,
-//     x: 0,
-//     pointerEvents: "all",
-//     stagger: 0.1,
-//     duration: 1, // Adjust duration as needed
-//     ease: "power1.inOut",
-//   }
-// );
-
-// // featureTl.to({}, { duration: 0.5 });
-
-// // // Reverse animation: Fade out and move elements back to the right
-// // featureTl.to(".feature-item", {
-// //   opacity: 0,
-// //   x: 100,
-// //   pointerEvents: "none",
-// //   stagger: { each: 0.1, from: "end" },
-// //   duration: 1, // Adjust duration as needed
-// //   ease: "power1.inOut",
-// // });
-
-// // System aniamtion
-// const systemTl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".hero-area",
-//     start: "2000 top",
-//     end: "+=2500", // Total duration combining both animations (1200px + 800px)
-//     scrub: 1,
-//   },
-// });
-
 (function ($) {
   "use strict";
-
-  //========== MAGNIFIC POPUP ==========>
-  $(".video-btn").magnificPopup({
-    type: "iframe",
-  });
-  $(".pop-img-btn").magnificPopup({
-    type: "image",
-  });
-  $(".gallery-item a").magnificPopup({
-    type: "image",
-    gallery: {
-      enabled: true,
-    },
-  });
-  //========== MAGNIFIC POPUP// ==========>
-
-  //========== ONE PAGE NAV ==========>
-  var top_offset = $(".header-area").height() - 170;
-  $(".mainmenu ul, .sidebar ul").onePageNav({
-    scrollOffset: top_offset,
-  });
-  //========== ONE PAGE NAV// ==========>
-
-  //========== SKROLLR JS ==========>
-  var skrollrSetting = {
-    forceHeight: false,
-    smoothScrollingDuration: 500,
-  };
-  var s = skrollr.init(skrollrSetting);
-  function doubleAction() {
-    if (window.matchMedia("(max-width: 991px)").matches) {
-      s.destroy();
-    } else {
-      s = skrollr.init(skrollrSetting);
-    }
-  }
-  window.addEventListener("load", () => {
-    doubleAction();
-  });
-  window.addEventListener("resize", () => {
-    doubleAction();
-  });
-  //========== SKROLLR JS// ==========>
 
   //========== CLIMATE SLIDER ==========>
 
@@ -986,10 +810,6 @@ lottieIcon.forEach((icon) => {
   climateSlider.on(
     "afterChange",
     function (event, slick, currentSlide, nextSlide) {
-      console.log(slick);
-      // console.log(slick);
-      // console.log(currentSlide);
-      // console.log(nextSlide);
       climateSlider_is_animating = false;
     }
   );
@@ -1019,49 +839,6 @@ lottieIcon.forEach((icon) => {
     fade: true,
   });
   //========== img SLIDER// ==========>
-
-  //========== NASA SLIDER ==========>
-  // $(".nasa-slider").slick({
-  //   slidesToShow: 4,
-  //   slidesToScroll: 1,
-  //   dots: true,
-  //   arrows: true,
-  //   swipeToSlide: true,
-  //   infinite: false,
-  //   appendDots: ".nasa-slider-dots",
-  //   appendArrows: ".nasa-slider-controller",
-  //   prevArrow:
-  //     '<button type="button" class="slick-prev"><i class="fal fa-angle-left"></i></button>',
-  //   nextArrow:
-  //     '<button type="button" class="slick-next"><i class="fal fa-angle-right"></i></button>',
-
-  //   responsive: [
-  //     {
-  //       breakpoint: 1200,
-  //       settings: {
-  //         slidesToShow: 4,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 991,
-  //       settings: {
-  //         slidesToShow: 3,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 767,
-  //       settings: {
-  //         slidesToShow: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
 
   //========== NASA SLIDER// ==========>
 })(jQuery);
